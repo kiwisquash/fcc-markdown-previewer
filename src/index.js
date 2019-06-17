b431a9f3d246d1dd255a72737addf95edad97587
Ki Song <kiwisquash@gmail.com>
2019-06-17 14:54:19 -0700

    Added some very basic structure to the markdown app.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+class Editor extends React.Component {
+
+	handleChange(e) {
+	}
+
+	render() {
+		return (
+			<textarea id="editor" onChange={this.props.onChange} value={this.props.value} />
+		);
+	}
+}
+
+function Preview(props) {
+	return (
+		<div id="preview">{props.value}</div>
+	)
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: "this is a test",
+		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleChange(event) {
+		this.setState({
+			text: event.target.value, 
+		})			
+	}
+
+	render() {
+		return (
+			<div id="app">
+				<Editor value={this.state.text} onChange={this.handleChange}/>
+				<Preview value={this.state.text}/>
+			</div>
+		)
+	}
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
